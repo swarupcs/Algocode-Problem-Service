93d9e324b46bf609ceb2a7b9602530c3ba694939
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2025-01-11 13:55:29 +0530

    Feat: Added error handler middleware with basic error classes

--- README.md
+++ README.md
@@ -11,3 +11,5 @@
     - because the route starts with /api
         /api      -> /v1      -> /problems     -> /ping
         apiRouter -> v1Router -> problemRouter -> problemController -> service layer
+
+
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-const express = require('express');
+const express = require("express");
 const bodyParser = require("body-parser");
 
-const { PORT } = require('./config/server.config');
+const { PORT } = require("./config/server.config");
 const apiRouter = require("./routes");
+const errorHandler = require("./utils/errorHandler");
 
 const app = express();
 
@@ -13,11 +14,13 @@ app.use(bodyParser.text());
 // If any request comes and route starts with /api, we map it to apiRouter
 app.use("/api", apiRouter);
 
-app.get('/ping', (req, res) => {
-    res.send({message: 'Problem Service is alive'});
+app.get("/ping", (req, res) => {
+  return res.json({ message: "Problem Service is alive" });
 });
+
+// last middleware if any error comes
+app.use(errorHandler);
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server started at PORT: ${PORT}`);
 });
-
